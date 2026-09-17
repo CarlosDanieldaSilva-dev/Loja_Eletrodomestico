233b97ee640fe1ddcc90225d6cc6627942d97bf3
CarlosDanieldaSilva-dev <carlos2000.tboa@gmail.com>
2026-09-17 20:02:58 -0300

    Teste consulta de produtos

--- teste-banco.php
+++ teste-banco.php
@@ -1,7 +1,20 @@
-<?php error_reporting(E_ALL);
+<?php
+
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 include(__DIR__ . "/conexao.php");
-echo "Conexão realizada com sucesso!<br>";
-$resultado = $conexao->query("SELECT COUNT(*) FROM produtos");
-$total = $resultado->fetchColumn();
-echo "Produtos encontrados: " . $total;
+
+echo "Conexão OK<br>";
+
+$resultado = $conexao->query("SELECT * FROM produtos LIMIT 1");
+
+echo "SELECT executado!<br>";
+
+$produto = $resultado->fetch(PDO::FETCH_ASSOC);
+
+echo "Produto encontrado:<br>";
+
+echo "<pre>";
+print_r($produto);
+echo "</pre>";
